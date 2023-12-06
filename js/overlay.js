e3c7031faedf23585b914a4b215c5989e5ad1dc4
Marcel <marcel.amrein@hotmail.com>
2023-12-06 15:26:30 +0100

    useless

--- js/overlay.js
+++ js/overlay.js
@@ -1,24 +1,19 @@
 // overlay.js
 
-// Function to show the overlay
 function showOverlay() {
     document.body.classList.add('overlay-open');
     document.getElementById('titleOverlay').classList.add('show');
 }
 
-// Function to hide the overlay and start transitions
 function hideOverlay() {
     document.body.classList.remove('overlay-open');
     document.getElementById('titleOverlay').classList.remove('show');
 }
 
-// Add event listener to close the overlay when clicking anywhere
 document.getElementById('titleOverlay').addEventListener('click', hideOverlay);
 
-// Add event listener to prevent closing the overlay when clicking inside the overlay content
 document.getElementById('titleOverlay').querySelector('div').addEventListener('click', function (event) {
     event.stopPropagation(); // Prevent the click event from reaching the overlay
 });
 
-// Call showOverlay() to display the overlay
 showOverlay();
